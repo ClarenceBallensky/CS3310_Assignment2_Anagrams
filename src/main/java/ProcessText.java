6c2b3c4f382e96aa13e4aa071861422e10842d69
Clarence Ballensky <alballensky@cpp.edu>
2025-11-03 18:26:48 -0800

    Cleaned up code

--- src/main/java/ProcessText.java
+++ src/main/java/ProcessText.java
@@ -2,7 +2,7 @@
  * Clarence Ballensky
  * CS 3310.01, Fall 2025
  * Programming assignment 2: anagrams
- * Parses through words in a given input file to identify if any of the words are anagrams of each other
+ * ProcessText class: Parses through words in a given input file to identify if any of the words are anagrams of each other
  */
 
 import java.io.*;
@@ -12,13 +12,14 @@ import java.io.FileNotFoundException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.text.Normalizer;
+import java.util.Arrays;
 
 
 public class ProcessText
 {
     /**
      * Method: main
-     * Purpose: parses through each word in the input file, groups all anagrams, saves to an output file
+     * Purpose: parses through each word in the input file, groups all anagrams, saves the groups to an output file
      */
     public static void main (String[] args) {
 
@@ -28,7 +29,10 @@ public class ProcessText
             throw new IllegalArgumentException("Please ONLY provide a file name as a command line argument.");
         }
 
+        //the String part stores the key, and stores the alphabetized version of a word
+        //the List<String> part stores all words that match up with the key (all anagrams within that group)
         HashMap<String, List<String>> anagramGroups = new HashMap<>();
+
         File inputFile = new File(args[0]); //opens up the file specified in the command line
 
 
@@ -39,9 +43,9 @@ public class ProcessText
                 if (word.isEmpty()) {
                     continue;
                 }
-                String sortedKey = sortCharacters(word);
+                String sortedKey = sortCharacters(word); //sorted version of the given word
 
-                if (!anagramGroups.containsKey(sortedKey))
+                if (!anagramGroups.containsKey(sortedKey)) //if the anagram group doesn't exist yet, make a new anagram group
                 {
                     anagramGroups.put(sortedKey, new ArrayList<>());
                 }
@@ -52,8 +56,10 @@ public class ProcessText
             System.out.println("Error: File not found --> " + args[0]);
         }
 
+        //creates output file
         String outputFilename = "C:\\Users\\prize\\IdeaProjects\\CS3310_Assignment2_Anagrams\\src\\main\\resources\\outputFile.txt";
 
+        //prints contents to output file
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilename)))
         {
             for ( List<String> group : anagramGroups.values())
@@ -79,7 +85,7 @@ public class ProcessText
 
     /**
      * Method: sortCharacters
-     * Purpose: sort the characters in a given string alphabetically and return the sorted string
+     * Purpose: normalize and ignore capitals for a given string, then sort it alphabetically and return the sorted string
      * @param word a given word from the input file
      * @return String(wordChars)
      */
@@ -90,6 +96,7 @@ public class ProcessText
         //e.g. "ä" becomes "a\u0308"
         String normalized = Normalizer.normalize(word, Normalizer.Form.NFD);
         normalized = normalized.replaceAll("\\p{M}", ""); //remove diacritical marks
+        normalized = normalized.replaceAll("[^\\p{IsAlphabetic}]", ""); //remove punctuation and special characters (leaves only letters)
         normalized = normalized.toLowerCase(); //make all characters lowercase
 
         char[] wordChars =  normalized.toCharArray(); //create a character array of each character in the word
